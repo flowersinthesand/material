439013c9f561b3d0169e3dcde37a78a48b781eec
Donghwan Kim <flowersinthesand@gmail.com>
2015-09-20 00:26:03 +0900

    First pages commit

---
+++ js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.4.4",
+  "version": "0.11.0",
+  "repository": "https://github.com/angular/material",
+  "commit": "d72053c0db0bb7dee1edb0fbcb933bff88f01410",
+  "date": "2015-09-19 23:20:28 +0900"
+});
